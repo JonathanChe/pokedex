0fa72a3cd739ab8e192f86fa3f1479d76087a2a7
Jonathan <jonathan.che@outlook.com>
2019-01-10 18:42:47 -0800

    store, reducers, constants, actions completed

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,9 +1,32 @@
-import * as ActionTypes from '../constants/constants';
+import * as actionTypes from '../constants/constants';
 
-export function selectedPokemon(pokemon) {
-  return {
-    type: ActionTypes.SELECTED_POKEMON,
-    payload: pokemon,
-  };
+function fetchingSinglePokemon(url) {
+  return fetch(url);
 }
 
+export const allPokemons = (pokemons) => ({
+  type: actionTypes.ALL_POKEMON,
+  payload: pokemons,
+});
+
+export const matchedPokemon = (matches) => ({
+  type: actionTypes.MATCHED_POKEMON,
+  payload: matches,
+});
+
+export const toggleShow = () => ({
+  type: actionTypes.TOGGLE_SHOW,
+});
+
+export const setShowToFalse = () => ({
+  type: actionTypes.SET_SHOW_TO_FALSE,
+});
+
+export const displayPokemonAsync = (url) => {
+  return function (dispatch) {
+    fetchingSinglePokemon(url)
+      .then(data => data.json())
+      .then(response => dispatch({ type: 'DISPLAY_POKEMON', payload: response}))
+      .catch(err => console.error(err))
+  }
+}
---
+++ client/reducers/reducers.js
@@ -0,0 +1,49 @@
+import { combineReducers } from 'redux';
+import * as actionTypes from '../constants/constants';
+import findMatches from '../../helpers';
+
+const initialState = {
+  allPokemons: [],
+  matchedPokemons: [],
+  show: false,
+  displayPokemon: {},
+}
+
+function pokemonReducer(state = initialState, action) {
+  switch (action.type) {
+    case actionTypes.ALL_POKEMON:
+      return {
+        ...state,
+        allPokemons: action.payload,
+      };
+    case actionTypes.TOGGLE_SHOW:
+      return {
+        ...state,
+        show: !state.show,
+      };
+    case actionTypes.MATCHED_POKEMON:
+      const filtered = findMatches(action.payload, state.allPokemons);
+      return {
+        ...state,
+        matchedPokemons: filtered,
+      }
+    case actionTypes.DISPLAY_POKEMON:
+      return {
+        ...state,
+        displayPokemon: action.payload,
+      };
+    case actionTypes.SET_SHOW_TO_FALSE:
+      return {
+        ...state,
+        show: false,
+      }
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  pokemonReducer,
+})
+
+export default rootReducer;
